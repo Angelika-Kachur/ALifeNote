5c98e9395508fc5aca9c73d602d48d556f1882aa
Angelika Kachur <angelika.k@team.anyforsoft.com>
2018-12-23 02:03:57 +0200

    add vue cli and create gratitude section

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="wrapper">
+      <div class="date">
+          Today is <span class="date-today">22.12.2018</span>
+      </div>
+
+      <section class="gratitude-section">
+          <h2>Tell Thanks..{{ message }}</h2>
+
+          <form class="gratitudes">
+              <textarea name="gratitude" id="" cols="30" rows="10"></textarea>
+              <button class="create-gratitude">Tell one more Thanks</button>
+          </form>
+
+          <div class="all-gratitudes">
+              <h3> Here are all your gratitudes.</h3>
+          </div>
+      </section>
+
+    </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Hello'
+      }
+    }
+  }
+</script>
+
+<style>
+  .wrapper {
+    padding: 10px;
+  }
+</style>
